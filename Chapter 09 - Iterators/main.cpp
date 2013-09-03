6a5fe6ac3093762afcedb8869bd6f1d2130db52b
Anton Golov <jesyspa@gmail.com>
2013-09-03 11:08:01 +0200

    Lowered the pace a little.

---
+++ Chapter 09 - Iterators/main.cpp
@@ -0,0 +1,5 @@
+/* Looking back over the chapter on references I felt like there needs to be a
+ * little more introduced before them; there were simply too many concepts
+ * focused on there.  This chapter is still under construction; feel free to
+ * skip ahead to chapter 11, but be warned that it is not particularly clear.
+ */
